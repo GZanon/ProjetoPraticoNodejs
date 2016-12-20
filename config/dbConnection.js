55283bdc79c49c40bca34118a49ddd89e264711d
Giovane Zanon <giovanezanon@gmail.com>
2016-12-20 16:32:44 -0200

    Seção 8

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function(){
+var connMySQL = function(){
+    console.log('Conexao com bd estabelecida');
     return mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : '1234',
         database : 'portal_noticias'
     });
+}
+
+module.exports = function(){
+    console.log('Autoload carregou o modulo da conexao com o bd');
+    return connMySQL;
 }
